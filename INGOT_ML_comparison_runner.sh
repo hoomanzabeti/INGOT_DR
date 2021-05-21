c91d8032f6e5aa283bc211fb72fad46d626a0e8f
Hooman Zabeti <hoomanzabeti@gmail.com>
2021-05-21 10:42:27 -0700

    Added results reproducing scripts

---
+++ INGOT_ML_comparison_runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+for DRUG in 'amikacin' 'capreomycin' 'ciprofloxacin' 'ethambutol' 'ethionamide' 'isoniazid' 'kanamycin' 'moxifloxacin' 'ofloxacin' 'pyrazinamide' 'rifampicin' 'streptomycin'
+do
+  for MODEL in 'INGOT' 'LR_l1' 'LR_l2' 'SVM_l1' 'SVM_l2' 'RF'
+  do
+
+    echo ${DRUG}
+    echo ${MODEL}
+    python INGOT_ML_comparison.py --config=config.yml --data-file=data/SNPsMatrix_${DRUG}.csv --label-file=data/${DRUG}Label.csv --drug-name=${DRUG} --model-name=${MODEL}
+  done
+done
